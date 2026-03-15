ad5000e9c0f783d582d014f54c45f3382ad109a2
caleb3873 <caleb@schlegelgreenhouse.com>
2026-03-14 20:44:58 -0400

    Update Export.jsx

--- src/Export.jsx
+++ src/Export.jsx
@@ -721,13 +721,6 @@ function getMovableHolidays(year) {
   return h;
 }
 
-function getWeekNumber(date) {
-  const d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
-  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
-  const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-  return Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
-}
-
 function WeekCalendarExport() {
   const currentYear = new Date().getFullYear();
   const [calYear,   setCalYear  ] = useState(currentYear);
